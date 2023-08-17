8dd052e5324e7e56b95619f5c231ec7b262c6f7a
Nancy Chaudhry <nancych14@gmail.com>
2023-08-17 12:12:28 +0500

    Host correction

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://task-mate.up.railway.app/"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "task-mate.up.railway.app/"]
 CSRF_TRUSTED_HOSTS = ["https://task-mate.up.railway.app"]
 
 
